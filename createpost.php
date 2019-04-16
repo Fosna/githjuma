c07895333dbb5fd599a9b4cb031bd50d1d628a48
JurajJausovec <48887455+JurajJausovec@users.noreply.github.com>
2019-04-16 21:35:26 +0200

    Creategroup/post style

--- createpost.php
+++ createpost.php
@@ -1,24 +1,22 @@
 <?php require 'header.php'; ?>
-  <link rel="stylesheet" href="style/creategroup.style.css">
-  <h1>Create post</h1>
-  <form class="" action="scr/createpost.scr.php" method="post" enctype="multipart/form-data">
-    <label for="name"><b>Title:</b></label>
-    <input class="groupinput" type="text" autocomplete="off"  placeholder="Name" name="title" value="">
-    <label for="description"><b>Description</b></label>
-    <input class="groupinput" type="text1" autocomplete="off"  placeholder="Description " name="description" value="">
-    <label for="maxmembers"><b>Username</b></label>
-
-
-    <label for="comments"><b>Comments</b></label>
-    <select class="select" name="comments">
-      <option value="allow">Allow</option>
-      <option value="dont allow">Dont allow</option>
-    </select>
-    <input type="file" name="avatar" value="">
-    <button type="submit" name="creategroup-submit"  class="publish">Create post</button>
-
-  </form>
-
+  <link rel="stylesheet" href="style/createpost.style.css">
+  <div class="box">
+    <h1>Create post</h1>
+    <form class="" action="scr/createpost.scr.php" method="post" enctype="multipart/form-data">
+      <label for="name"></label>
+      <input class="groupinput" type="text" autocomplete="off"  placeholder="Name" name="title" value="">
+      <label for="description"></label>
+      <input class="groupinput" type="text1" autocomplete="off"  placeholder="Description " name="description" value="">
+      <label for="maxmembers"></label>
+      <label for="comments"></label>
+      <select class="select" name="comments">
+        <option value="allow">Allow</option>
+        <option value="dont allow">Dont allow</option>
+      </select>
+      <input type="file" name="avatar" value="">
+      <button type="submit" name="creategroup-submit"  class="publish">Create post</button>
+    </form>
+  </div>
 <?php
 require 'footer.php';
 ?>
