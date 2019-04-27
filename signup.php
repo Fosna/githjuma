716850bdec90547b9181f0fb63ec49af59999429
Ian Palisak <42089208+Burnbag@users.noreply.github.com>
2019-04-27 23:50:59 +0200

    icon

--- signup.php
+++ signup.php
@@ -2,34 +2,48 @@
 <html>
   <head>
     <title>Hjuma</title>
-    <link rel="stylesheet" href="style/signup.style.css">
+    <link rel="stylesheet" href="style/login_signup.style.css">
   </head>
   <body>
     <?php require "header.php"; ?>
       <form class="content" action="scr/signup.scr.php" method="post">
+<div class="container">
+        <h1 style="text-align: center; padding: 60px;">Sign up</h1>
 
-        <h1>Registracija</h1>
 
-        <div class="container">
         <?php
           if (isset($_GET['username'])) {
             $username = $_GET['username'];
-            echo '<input type="text" name="username" autocomplete="off" placeholder="Korisničko ime" size="30" value="'.$username.'" required/>';
+            echo '<input type="text" class="form-control" name="username" autocomplete="off" placeholder="Korisničko ime" size="30" value="'.$username.'" required/>';
           }
           else {
-            echo '<input type="text" name="username" autocomplete="off" placeholder="Korisničko ime" size="30" value="" required/>';
+            echo '
+            <div class="form-group">
+            <input type="text" name="username" class="form-control" autocomplete="off" placeholder="Korisničko ime" size="30" value="" required/>
+            </div>';
           }
           if (isset($_GET['email'])) {
             $email = $_GET['email'];
-            echo '<input type="text" name="email" autocomplete="off" placeholder="E-mail" size="30" value="'.$email.'" required/>';
+            echo '
+            <div class="form-group">
+            <input type="text" name="email" class="form-control" autocomplete="off" placeholder="E-mail" size="30" value="'.$email.'" required/>
+            </div>';
           }
           else {
-            echo '<input type="text" name="email" autocomplete="off" placeholder="E-mail" size="30" value="" required/>';
+            echo '
+            <div class="form-group">
+            <input type="text" name="email"class="form-control" autocomplete="off" placeholder="E-mail" size="30" value="" required/>
+            </div>
+            ';
           }
         ?>
-        <input type="password" name="password" autocomplete="off" placeholder="Lozinka" size="30" value="" required/>
-        <input type="password" name="password-rp" autocomplete="off" placeholder="Potvrda lozinke" size="30" value="" required/>
-        <button type="submit" name="signup-submit" class="submit" autocomplete="off" value="Submit">Registracija</button>
+        <div class="form-group">
+        <input type="password" name="password" class="form-control" autocomplete="off" placeholder="Lozinka" size="30" value="" required/>
+      </div>
+      <div class="form-group">
+        <input type="password" name="password-rp" class="form-control" autocomplete="off" placeholder="Potvrda lozinke" size="30" value="" required/>
+      </div>
+        <button type="submit" name="signup-submit" class="btn btn-primary" autocomplete="off" value="Submit">Registracija</button>
         <a class="forgotenPassword" href="#">Forgoten password?</a>
         </div>
       </form>
