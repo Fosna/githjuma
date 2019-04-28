9d15dd75b7281f564245a4254a7d069db336d012
JurajJausovec <48887455+JurajJausovec@users.noreply.github.com>
2019-04-28 20:39:57 +0200

    changename style

--- changename.php
+++ changename.php
@@ -1,14 +1,16 @@
 <?php require "header.php"; ?>
-<link rel="stylesheet" href="style/changename.style.css">
+<link rel="stylesheet" href="style/login_signup.style.css">
 <link rel="stylesheet" href="style/includes/error.inc.css">
 <body>
 <h1>Change name</h1>
 
-  <div class="changename_container">
+  <div class="container">
     <h1 class="newname">New name</h1>
     <form class="" action="scr/changename.scr.php" method="post">
-      <input class="input_newname" type="text" name="newname" value="" required>
-      <button type="submit" class="submitbtn" name="newname-submit">Change</button>
+      <div class="form-group">
+      <input class="form-control" type="text" name="newname" value="" required>
+    </div>
+      <button type="submit" class="btn btn-success" style="display: block; margin-left: auto; margin-right:auto; font-size: 30px;" name="newname-submit">Change</button>
     </form>
   </div>
   <?php
