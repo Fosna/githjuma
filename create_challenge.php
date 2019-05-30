2d79c462d1a1aae715deace6798fe1b300f5c377
JurajJausovec <48887455+JurajJausovec@users.noreply.github.com>
2019-05-30 19:55:38 +0200

    start create challlenge

---
+++ create_challenge.php
@@ -0,0 +1,40 @@
+<?php require 'header.php'; ?>
+<div  class="header_space"></div> 
+<link rel="stylesheet" href="style/create_challenge.style.css">
+<form class="content" action="scr/create_challenge.scr.php" method="post">
+  <div class="container">
+    <h1>Create challenge</h1>
+    <hr class="my-2">
+    <div class="form-group">
+      <label for="username">Title</label>
+      <input type="text" class="form-control" name="title" aria-describedby="title" placeholder="Title of a challenge">
+    </div>
+    <div class="form-group">
+      <label for="password">Description</label>
+      <input type="password" class="form-control" name="description" placeholder="Description of challenge">
+    </div>
+    <div class="form-group">
+      <label for="sel1">Select programming language:</label>
+      <select name="prog_language" class="form-control" id="sel1">
+        <option value="python">Python</option>
+        <option value="php">PHP</option>
+        <option value="c">C</option>
+        <option value="c++">C++</option>
+        <option value="c#">C#</option>
+        <option value="javascript">JavaScript</option>
+        <option value="java">Java</option>
+        <option value="html_css">HTML&CSS</option>
+      </select>
+      </div>
+      <div class="form-group">
+      <label for="password">Start date:</label>
+      <input type="date" class="form-control" name="start_date">
+    </div>
+    <div class="form-group">
+      <label for="password">Deadline</label>
+      <input type="date" class="form-control" name="deadline">
+    </div>
+    <button type="submit" class="btn btn-success btn-block" name="create_challenge-submit">Create Challenge!</button>
+   
+  </div>
+</form>
---
+++ scr/create_challenge.scr.php
@@ -0,0 +1,35 @@
+<?php
+if (!isset($_POST['create_challenge-submit'])) {
+  exit();
+}
+elseif (isset($_POST['create_challenge-submit'])) {
+  require 'dbh.scr.php';
+
+  $title = mysqli_real_escape_string($conn, $_POST['title']);
+  $description = mysqli_real_escape_string($conn, $_POST['description']);
+  $prog_language = mysqli_real_escape_string($conn, $_POST['prog_language']);
+  $start_date = mysqli_real_escape_string($conn, $_POST['start_date']);
+  $deadline = mysqli_real_escape_string($conn, $_POST['deadline']);
+
+  if (empty($title) || empty($description) || empty($prog_language) || empty($start_date) || empty($deadline)){
+    header("Location: ../create_challenge?error=empty");
+    exit();
+  }
+  else {
+    $sql = "INSERT INTO hjuma_challenges (title, description, prog_language, start_date, deadline) VALUES (?, ?, ?, ?, ?, ?, '$image');";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      echo "SQL error";
+    }else {
+      mysqli_stmt_bind_param($stmt,"sssss", $title, $description, $prog_language, $start_date, $deadline);
+      mysqli_stmt_execute($stmt);
+    }
+    header("Location: ../comments");
+
+    }
+  }
+}
+else {
+  header("Location: ../main");
+  exit();
+}
