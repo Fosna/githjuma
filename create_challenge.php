4eabe26c9f00b7939fd12db3a0c6c29f2112f901
JurajJausovec <48887455+JurajJausovec@users.noreply.github.com>
2019-05-31 18:15:07 +0200

    d

--- create_challenge.php
+++ create_challenge.php
@@ -6,16 +6,16 @@
     <h1>Create challenge</h1>
     <hr class="my-2">
     <div class="form-group">
-      <label for="username">Title</label>
-      <input type="text" class="form-control" name="title" aria-describedby="title" placeholder="Title of a challenge">
+      <label for="username">Title:</label>
+      <input type="text" class="form-control" name="challenge_title" aria-describedby="title" placeholder="Title of a challenge">
     </div>
     <div class="form-group">
-      <label for="password">Description</label>
-      <input type="text" class="form-control" name="description" placeholder="Description of challenge">
+      <label for="password">Description:</label>
+      <input type="text" class="form-control" name="challenge_description" placeholder="Description of challenge">
     </div>
     <div class="form-group">
       <label for="sel1">Select programming language:</label>
-      <select name="prog_language" class="form-control" id="sel1">
+      <select name="challenge_prog_language" class="form-control" id="sel1">
         <option value="python">Python</option>
         <option value="php">PHP</option>
         <option value="c">C</option>
@@ -28,11 +28,15 @@
       </div>
       <div class="form-group">
       <label for="password">Start date:</label>
-      <input type="date" class="form-control" name="start_date">
+      <input type="date" class="form-control" name="challenge_start_date">
     </div>
     <div class="form-group">
       <label for="password">Deadline</label>
-      <input type="date" class="form-control" name="deadline">
+      <input type="date" class="form-control" name="challenge_deadline">
+    </div>
+    <div class="form-group">
+      <label for="password">Pasword:</label>
+      <input type="password" class="form-control" name="challenge_password" placeholder="Password of challenge">
     </div>
     <button type="submit" class="btn btn-success btn-block" name="create_challenge-submit">Create Challenge!</button>
    
--- scr/create_challenge.scr.php
+++ scr/create_challenge.scr.php
@@ -5,23 +5,24 @@ if (!isset($_POST['create_challenge-submit'])) {
 elseif (isset($_POST['create_challenge-submit'])) {
   require 'dbh.scr.php';
 
-  $title = mysqli_real_escape_string($conn, $_POST['title']);
-  $description = mysqli_real_escape_string($conn, $_POST['description']);
-  $prog_language = mysqli_real_escape_string($conn, $_POST['prog_language']);
-  $start_date = mysqli_real_escape_string($conn, $_POST['start_date']);
-  $deadline = mysqli_real_escape_string($conn, $_POST['deadline']);
-
-  if (empty($title) || empty($description) || empty($prog_language) || empty($start_date) || empty($deadline)){
+  $challenge_title = mysqli_real_escape_string($conn, $_POST['challenge_title']);
+  $challenge_description = mysqli_real_escape_string($conn, $_POST['challenge_description']);
+  $challenge_prog_language = mysqli_real_escape_string($conn, $_POST['challenge_prog_language']);
+  $challenge_start_date = mysqli_real_escape_string($conn, $_POST['challenge_start_date']);
+  $challenge_deadline = mysqli_real_escape_string($conn, $_POST['challenge_deadline']);
+  $challenge_password = mysqli_real_escape_string($conn, $_POST['challenge_password']);
+  if (empty($challenge_title) || empty($challenge_description) || empty($challenge_prog_language) || empty($challenge_start_date) || empty($challenge_deadline)){
     header("Location: ../create_challenge?error=empty");
     exit();
   }
   else {
-    $sql = "INSERT INTO hjuma_challenges (challenge_title, challenge_description, challenge_prog_language, challenge_start_date, challenge_deadline) VALUES (?, ?, ?, ?, ?);";
+    $hashedPwd = password_hash($challenge_password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO hjuma_challenges (challenge_title, challenge_description, challenge_prog_language, challenge_start_date, challenge_deadline, challenge_password) VALUES (?, ?, ?, ?, ?, ?);";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
       echo "SQL error";
     }else {
-      mysqli_stmt_bind_param($stmt,"sssss", $title, $description, $prog_language, $start_date, $deadline);
+      mysqli_stmt_bind_param($stmt,"ssssss", $challenge_title, $challenge_description, $challenge_prog_language, $challenge_start_date, $challenge_deadline, $hashedPwd);
       mysqli_stmt_execute($stmt);
     }
     header("Location: ../main");
