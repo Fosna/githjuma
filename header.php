07899248685108cec2dba5e9f51afc0d965bbe35
Ian Palisak <42089208+Burnbag@users.noreply.github.com>
2019-07-26 00:43:02 +0200

    icon

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
   <link rel="stylesheet" href="style/header.style.css">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="pics/favicon.ico" type="image/icon type">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
